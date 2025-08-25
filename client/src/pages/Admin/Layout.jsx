e44c3949e2c6a5eaf7eacf7c07795c86b86c60f2
Anshul1076 <anshubasu9@gmail.com>
2025-08-26 01:54:28 +0530

    some changes

--- client/src/pages/Admin/Layout.jsx
+++ client/src/pages/Admin/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import AdminNavbar from '../../components/Admin/AdminNavbar'
-import AdminSidebar from '../../components/Admin/AdminSidebar'
+import AdminNavbar from '../../components/admin/AdminNavbar'
+import AdminSidebar from '../../components/admin/AdminSidebar'
 import { Outlet } from 'react-router-dom'
 import { useAppContext } from '../../context/AppContext'
 import Loading from '../../components/Loading'
