addfaa786e7f97f074f3d64b1549ffc37763b007
Anshul1076 <anshubasu9@gmail.com>
2025-08-26 03:00:18 +0530

    change in blur component

--- client/src/components/BlurCircle.jsx
+++ client/src/components/BlurCircle.jsx
@@ -2,7 +2,7 @@
 const BlurCircle = ({top = "auto", left = "auto", right = "auto", bottom = "auto"}) => {
   return (
     <div className="absolute -z-50 h-58 w-58 aspect-square
-    rounded-full bg-primary/30 blur-3xl"
+    rounded-full bg-primary/30 blur-4xl"
     style={ {top: top, left: left, right: right, bottom: bottom} }>
         
     </div>
